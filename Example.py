6db4d2e60bd1dc24f570d9cc366c882234486160
Username <codrinaignat@hotmail.com>
2023-10-14 12:15:43 +0300

    2 apps done

---
+++ Example.py
@@ -0,0 +1,47 @@
+import tkinter as tk
+
+userInt = tk.Tk()
+userInt.geometry("500x500")
+userInt.title("My first GUI")
+label = tk.Label(userInt, text="Hello world!", font=('Arial', 18))
+label.pack(padx=20, pady=20)
+
+textbox = tk.Text(userInt, height=3, font=('Arial', 16))
+textbox.pack(padx=10, pady=10)
+
+#button = tk.Button(root, text="Click me!", font=('Arial', 18))
+#button.pack(padx=10, pady=10)
+
+buttonFrame = tk.Frame(userInt)
+buttonFrame.columnconfigure(0, weight=1)
+buttonFrame.columnconfigure(1, weight=1)
+buttonFrame.columnconfigure(2, weight=1)
+
+btn1 = tk.Button(buttonFrame, text="1", font=('Arial', 18))
+btn1.grid(row=0, column=0, sticky=tk.W+tk.E)
+
+btn2 = tk.Button(buttonFrame, text="2", font=('Arial', 18))
+btn2.grid(row=0, column=1, sticky=tk.W+tk.E)
+
+btn3 = tk.Button(buttonFrame, text="3", font=('Arial', 18))
+btn3.grid(row=0, column=2, sticky=tk.W+tk.E)
+
+btn4 = tk.Button(buttonFrame, text="4", font=('Arial', 18))
+btn4.grid(row=1, column=0, sticky=tk.W+tk.E)
+
+btn5 = tk.Button(buttonFrame, text="5", font=('Arial', 18))
+btn5.grid(row=1, column=1, sticky=tk.W+tk.E)
+
+btn6 = tk.Button(buttonFrame, text="6", font=('Arial', 18))
+btn6.grid(row=1, column=2, sticky=tk.W+tk.E)
+
+buttonFrame.pack(fill='x')
+
+anotherButton = tk.Button(userInt, text="TEST")
+anotherButton.place(x=200, y=200, height=100, width=100)
+anotherButton.pack()
+
+#myentry = tk.Entry(root)
+#myentry.pack()
+
+userInt.mainloop()
