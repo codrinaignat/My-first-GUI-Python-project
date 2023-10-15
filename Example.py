65ab5e1381072cb68c32ef980af986f0ad47088e
Username <codrinaignat@hotmail.com>
2023-10-15 10:35:24 +0300

    Add new buttons, modify font

--- Example.py
+++ Example.py
@@ -1,9 +1,9 @@
 import tkinter as tk
 
 userInt = tk.Tk()
-userInt.geometry("500x500")
-userInt.title("My first GUI")
-label = tk.Label(userInt, text="Hello world!", font=('Arial', 18))
+userInt.geometry("500x400")
+userInt.title("My calculator")
+label = tk.Label(userInt, text="My calculator", font=('Times New Roman', 18))
 label.pack(padx=20, pady=20)
 
 textbox = tk.Text(userInt, height=3, font=('Arial', 16))
@@ -17,29 +17,52 @@ buttonFrame.columnconfigure(0, weight=1)
 buttonFrame.columnconfigure(1, weight=1)
 buttonFrame.columnconfigure(2, weight=1)
 
-btn1 = tk.Button(buttonFrame, text="1", font=('Arial', 18))
+btn1 = tk.Button(buttonFrame, text="1", font=('Times New Roman', 18))
 btn1.grid(row=0, column=0, sticky=tk.W+tk.E)
 
-btn2 = tk.Button(buttonFrame, text="2", font=('Arial', 18))
+btn2 = tk.Button(buttonFrame, text="2", font=('Times New Roman', 18))
 btn2.grid(row=0, column=1, sticky=tk.W+tk.E)
 
-btn3 = tk.Button(buttonFrame, text="3", font=('Arial', 18))
+btn3 = tk.Button(buttonFrame, text="3", font=('Times New Roman', 18))
 btn3.grid(row=0, column=2, sticky=tk.W+tk.E)
 
-btn4 = tk.Button(buttonFrame, text="4", font=('Arial', 18))
+btn4 = tk.Button(buttonFrame, text="4", font=('Times New Roman', 18))
 btn4.grid(row=1, column=0, sticky=tk.W+tk.E)
 
-btn5 = tk.Button(buttonFrame, text="5", font=('Arial', 18))
+btn5 = tk.Button(buttonFrame, text="5", font=('Times New Roman', 18))
 btn5.grid(row=1, column=1, sticky=tk.W+tk.E)
 
-btn6 = tk.Button(buttonFrame, text="6", font=('Arial', 18))
+btn6 = tk.Button(buttonFrame, text="6", font=('Times New Roman', 18))
 btn6.grid(row=1, column=2, sticky=tk.W+tk.E)
 
-buttonFrame.pack(fill='x')
+btn4 = tk.Button(buttonFrame, text="7", font=('Times New Roman', 18))
+btn4.grid(row=2, column=0, sticky=tk.W+tk.E)
 
-anotherButton = tk.Button(userInt, text="TEST")
-anotherButton.place(x=200, y=200, height=100, width=100)
-anotherButton.pack()
+btn5 = tk.Button(buttonFrame, text="8", font=('Times New Roman', 18))
+btn5.grid(row=2, column=1, sticky=tk.W+tk.E)
+
+btn6 = tk.Button(buttonFrame, text="9", font=('Times New Roman', 18))
+btn6.grid(row=2, column=2, sticky=tk.W+tk.E)
+
+btn4 = tk.Button(buttonFrame, text="0", font=('Times New Roman', 18))
+btn4.grid(row=3, column=0, sticky=tk.W+tk.E)
+
+btn5 = tk.Button(buttonFrame, text="+", font=('Times New Roman', 18))
+btn5.grid(row=3, column=1, sticky=tk.W+tk.E)
+
+btn6 = tk.Button(buttonFrame, text="-", font=('Times New Roman', 18))
+btn6.grid(row=3, column=2, sticky=tk.W+tk.E)
+
+btn6 = tk.Button(buttonFrame, text="*", font=('Times New Roman', 18))
+btn6.grid(row=4, column=0, sticky=tk.W+tk.E)
+
+btn6 = tk.Button(buttonFrame, text="/", font=('Times New Roman', 18))
+btn6.grid(row=4, column=1, sticky=tk.W+tk.E)
+
+btn6 = tk.Button(buttonFrame, text="=", font=('Times New Roman', 18))
+btn6.grid(row=4, column=2, sticky=tk.W+tk.E)
+
+buttonFrame.pack(fill='x')
 
 #myentry = tk.Entry(root)
 #myentry.pack()
