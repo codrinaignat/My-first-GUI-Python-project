d15e8a938fe992b9139c864126d1dcd1c4c2f4dd
Username <codrinaignat@hotmail.com>
2023-10-18 09:48:46 +0300

    Edit buttons borders

--- Example.py
+++ Example.py
@@ -18,49 +18,49 @@ buttonFrame.columnconfigure(0, weight=1)
 buttonFrame.columnconfigure(1, weight=1)
 buttonFrame.columnconfigure(2, weight=1)
 
-btn1 = tk.Button(buttonFrame, text="1", font=('Times New Roman', 18, 'bold'), bg='#E4C8D4')
+btn1 = tk.Button(buttonFrame, text="1", font=('Times New Roman', 18, 'bold'), bg='#E4C8D4', border='3')
 btn1.grid(row=0, column=0, sticky=tk.W+tk.E)
 
-btn2 = tk.Button(buttonFrame, text="2", font=('Times New Roman', 18, 'bold'), bg='#E4C8D4')
+btn2 = tk.Button(buttonFrame, text="2", font=('Times New Roman', 18, 'bold'), bg='#E4C8D4', border='3')
 btn2.grid(row=0, column=1, sticky=tk.W+tk.E)
 
-btn3 = tk.Button(buttonFrame, text="3", font=('Times New Roman', 18, 'bold'), bg='#E4C8D4')
+btn3 = tk.Button(buttonFrame, text="3", font=('Times New Roman', 18, 'bold'), bg='#E4C8D4', border='3')
 btn3.grid(row=0, column=2, sticky=tk.W+tk.E)
 
-btn4 = tk.Button(buttonFrame, text="4", font=('Times New Roman', 18, 'bold'), bg='#E4C8D4')
+btn4 = tk.Button(buttonFrame, text="4", font=('Times New Roman', 18, 'bold'), bg='#E4C8D4', border='3')
 btn4.grid(row=1, column=0, sticky=tk.W+tk.E)
 
-btn5 = tk.Button(buttonFrame, text="5", font=('Times New Roman', 18, 'bold'), bg='#E4C8D4')
+btn5 = tk.Button(buttonFrame, text="5", font=('Times New Roman', 18, 'bold'), bg='#E4C8D4', border='3')
 btn5.grid(row=1, column=1, sticky=tk.W+tk.E)
 
-btn6 = tk.Button(buttonFrame, text="6", font=('Times New Roman', 18, 'bold'), bg='#E4C8D4')
+btn6 = tk.Button(buttonFrame, text="6", font=('Times New Roman', 18, 'bold'), bg='#E4C8D4', border='3')
 btn6.grid(row=1, column=2, sticky=tk.W+tk.E)
 
-btn4 = tk.Button(buttonFrame, text="7", font=('Times New Roman', 18, 'bold'), bg='#E4C8D4')
+btn4 = tk.Button(buttonFrame, text="7", font=('Times New Roman', 18, 'bold'), bg='#E4C8D4', border='3')
 btn4.grid(row=2, column=0, sticky=tk.W+tk.E)
 
-btn5 = tk.Button(buttonFrame, text="8", font=('Times New Roman', 18, 'bold'), bg='#E4C8D4')
+btn5 = tk.Button(buttonFrame, text="8", font=('Times New Roman', 18, 'bold'), bg='#E4C8D4', border='3')
 btn5.grid(row=2, column=1, sticky=tk.W+tk.E)
 
-btn6 = tk.Button(buttonFrame, text="9", font=('Times New Roman', 18, 'bold'), bg='#E4C8D4')
+btn6 = tk.Button(buttonFrame, text="9", font=('Times New Roman', 18, 'bold'), bg='#E4C8D4', border='3')
 btn6.grid(row=2, column=2, sticky=tk.W+tk.E)
 
-btn4 = tk.Button(buttonFrame, text="0", font=('Times New Roman', 18, 'bold'), bg='#E4C8D4')
+btn4 = tk.Button(buttonFrame, text="0", font=('Times New Roman', 18, 'bold'), bg='#E4C8D4', border='3')
 btn4.grid(row=3, column=0, sticky=tk.W+tk.E)
 
-btn5 = tk.Button(buttonFrame, text="+", font=('Times New Roman', 18, 'bold'), bg='#B3EB8A')
+btn5 = tk.Button(buttonFrame, text="+", font=('Times New Roman', 18, 'bold'), bg='#B3EB8A', border='3')
 btn5.grid(row=3, column=1, sticky=tk.W+tk.E)
 
-btn6 = tk.Button(buttonFrame, text="-", font=('Times New Roman', 18, 'bold'), bg='#B3EB8A')
+btn6 = tk.Button(buttonFrame, text="-", font=('Times New Roman', 18, 'bold'), bg='#B3EB8A', border='3')
 btn6.grid(row=3, column=2, sticky=tk.W+tk.E)
 
-btn6 = tk.Button(buttonFrame, text="*", font=('Times New Roman', 18, 'bold'), bg='#B3EB8A')
+btn6 = tk.Button(buttonFrame, text="*", font=('Times New Roman', 18, 'bold'), bg='#B3EB8A', border='3')
 btn6.grid(row=4, column=0, sticky=tk.W+tk.E)
 
-btn6 = tk.Button(buttonFrame, text="/", font=('Times New Roman', 18, 'bold'), bg='#B3EB8A')
+btn6 = tk.Button(buttonFrame, text="/", font=('Times New Roman', 18, 'bold'), bg='#B3EB8A', border='3')
 btn6.grid(row=4, column=1, sticky=tk.W+tk.E)
 
-btn6 = tk.Button(buttonFrame, text="=", font=('Times New Roman', 18, 'bold'), bg='#B3EB8A')
+btn6 = tk.Button(buttonFrame, text="=", font=('Times New Roman', 18, 'bold'), bg='#B3EB8A', border='3')
 btn6.grid(row=4, column=2, sticky=tk.W+tk.E)
 
 buttonFrame.pack(fill='x')
